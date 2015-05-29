13b7e922b7c29a35a0a2442affd4a40ad85f12ca
RasAlhague <alexgr.dev@gmail.com>
2015-05-29 22:35:23 +0300

    Add properties

--- src/main/java/com/rasalhague/ereminisce/connection/EvernoteSession.java
+++ src/main/java/com/rasalhague/ereminisce/connection/EvernoteSession.java
@@ -8,26 +8,34 @@ import com.evernote.edam.error.EDAMSystemException;
 import com.evernote.edam.error.EDAMUserException;
 import com.evernote.thrift.TException;
 import com.rasalhague.ereminisce.Utils;
+import com.rasalhague.ereminisce.properties.Properties;
+import com.rasalhague.ereminisce.properties.PropertiesNames;
 import org.apache.log4j.Logger;
+
+import java.util.HashMap;
 
 public class EvernoteSession
 {
     private final static Logger          logger          = Logger.getLogger(EvernoteSession.class);
-    private final        long            RETRY_DELAY     = 5000;
+    private long retryDelay = 300000; // 5min
     private              String          devToken        = "S=s1:U=90da5:E=154bd566f51:C=14d65a54318:P=1cd:A=en-devtoken:V=2:H=6255af9a78934f41a9274f698651f05e";
     private              EvernoteService evernoteService = EvernoteService.SANDBOX;
     private EvernoteAuth evernoteAuth;
 
-    public EvernoteSession()
+    public EvernoteSession(Properties properties)
     {
         evernoteAuth = new EvernoteAuth(evernoteService, devToken);
+
+        setUpFieldsFromProperties(properties);
     }
 
-    public EvernoteSession(EvernoteService evernoteService, String devToken)
+    public EvernoteSession(Properties properties, EvernoteService evernoteService, String devToken)
     {
         this.evernoteService = evernoteService;
         this.evernoteAuth = new EvernoteAuth(evernoteService, devToken);
         this.devToken = devToken;
+
+        setUpFieldsFromProperties(properties);
     }
 
     public NoteStoreClient open()
@@ -43,7 +51,7 @@ public class EvernoteSession
 
             try
             {
-                Thread.sleep(RETRY_DELAY);
+                Thread.sleep(retryDelay);
                 return open();
             }
             catch (InterruptedException e1)
@@ -54,5 +62,24 @@ public class EvernoteSession
 
         //in normal situation program will never rich this point
         return null;
+    }
+
+    private void setUpFieldsFromProperties(Properties properties)
+    {
+        try
+        {
+            HashMap<String, String> propertiesMap = properties.getPropertiesMap();
+
+            if (propertiesMap.containsKey(PropertiesNames.ON_EXCEPTION_RETRY_DELAY))
+            {
+                long parseLong = Long.parseLong(propertiesMap.get(PropertiesNames.ON_EXCEPTION_RETRY_DELAY));
+                retryDelay = parseLong;
+                logger.info(PropertiesNames.ON_EXCEPTION_RETRY_DELAY + " set up to " + parseLong);
+            }
+        }
+        catch (Exception e)
+        {
+            logger.info(Utils.getStackTraceString(e));
+        }
     }
 }
